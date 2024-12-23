aae4f94d75be3067ba4273878d35e587c2cfda3a
Matias Vivas <106337651+MtVivas@users.noreply.github.com>
2024-12-23 02:11:34 -0300

    detalles de colores

--- script.js
+++ script.js
@@ -28,15 +28,12 @@ const productos = [
         descripcion: "Con una masa que contiene un 10% de harina integral y una fermentación natural de 24 horas. Su relleno incluye queso de girasol, tomates asados, albahaca fresca y cebollas caramelizadas."
     }
 ];
-
 // Función para cargar los productos
 function cargarProductos() {
     const productosContainer = document.getElementById("productos-container");
-
     productos.forEach((producto) => {
         const productoDiv = document.createElement("div");
         productoDiv.classList.add("producto");
-
         productoDiv.innerHTML = `
             <img src="${producto.imagen}" alt="${producto.nombre}">
             <p>${producto.nombre}</p>
@@ -47,16 +44,13 @@ function cargarProductos() {
                 ${producto.descripcion}
             </p>
         `;
-
         productosContainer.appendChild(productoDiv);
     });
 }
-
 // Función para alternar la visibilidad de la descripción
 function toggleInfo(id) {
     const descripcion = document.getElementById(`descripcion-${id}`);
     const infoBtn = document.getElementById(`info-btn-${id}`);
-
     if (descripcion.style.display === "none") {
         descripcion.style.display = "block";
         infoBtn.textContent = "Menos Info";
@@ -65,22 +59,18 @@ function toggleInfo(id) {
         infoBtn.textContent = "Más Info";
     }
 }
-
 // Carrito de compras
 let carrito = [];
-
 // Función para agregar productos al carrito
 function agregarAlCarrito(id) {
     const producto = productos.find(p => p.id === id);
     carrito.push(producto);
     actualizarCarrito();
 }
-
 // Función para actualizar el carrito en la interfaz
 function actualizarCarrito() {
     const carritoLista = document.getElementById("productos-carrito");
     carritoLista.innerHTML = ""; // Limpiar el carrito
-
     let total = 0;
     carrito.forEach((producto, index) => {
         const li = document.createElement("li");
@@ -91,22 +81,18 @@ function actualizarCarrito() {
         carritoLista.appendChild(li);
         total += producto.precio;
     });
-
     document.getElementById("total-carrito").innerText = `Total: $${total}`;
 }
-
 // Función para eliminar productos del carrito
 function eliminarDelCarrito(index) {
     carrito.splice(index, 1);
     actualizarCarrito();
 }
-
 // Función para vaciar el carrito
 function vaciarCarrito() {
     carrito = [];
     actualizarCarrito();
 }
-
 // Función para verificar la compra
 function verificarCompra() {
     if (carrito.length > 0) {
@@ -116,6 +102,5 @@ function verificarCompra() {
         alert("Tu carrito está vacío.");
     }
 }
-
 // Cargar productos al cargar la página
 window.onload = cargarProductos;
